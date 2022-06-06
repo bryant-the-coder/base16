efaac4eb75ed56bdb478220485f90b159fc2b2f9
bryant-the-coder <bryantfoo99@gmail.com>
2022-06-06 16:51:05 +0800

    fix decay theme

--- lua/themes/decay-base16.lua
+++ lua/themes/decay-base16.lua
@@ -1,18 +1,18 @@
 return {
-    base00 = "#171B20", -- bg
-    base01 = "#b6beca", -- lightbg
-    base02 = "#22262e", -- selection bg
-    base03 = "#242931", -- comments, line highlighting
-    base04 = "#15191d", -- dark foreground
-    base05 = "#dee1e6", -- Default Foreground, Caret, Delimiters, Operators
-    base06 = "#fafdff", -- Light Foreground (Not often used)
-    base07 = "#f5f5f5", -- Light Background (Not often used)
-    base08 = "#c68aee", -- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-    base09 = "#e05f65", -- Integers, Boolean, Constants, XML Attributes, Markup Link Url
-    base0A = "#7ddac5", -- Classes, Markup Bold, Search Text Background
-    base0B = "#f1cf8a", -- Strings, Inherited Class, Markup Code, Diff Inserted
-    base0C = "#74bee9", -- Support, Regular Expressions, Escape Characters, Markup Quotes
-    base0D = "#70a5eb", -- Functions, Methods, Attribute IDs, Headings
-    base0E = "#78DBA9", -- Keywords, Storage, Selector, Markup Italic, Diff Changed
-    base0F = "#a9b1d6", -- Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+    base00 = "#171B20",
+    base01 = "#21262e",
+    base02 = "#242931",
+    base03 = "#485263",
+    base04 = "#485263",
+    base05 = "#b6beca",
+    base06 = "#dee1e6",
+    base07 = "#dee1e6",
+    base08 = "#e05f65",
+    base09 = "#e9a180",
+    base0A = "#f1cf8a",
+    base0B = "#78DBA9",
+    base0C = "#9cd1ff",
+    base0D = "#74bee9",
+    base0E = "#c68aee",
+    base0F = "#e05f65",
 }
