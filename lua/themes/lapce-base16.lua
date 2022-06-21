e7e70de2a697d3f6f925fce48d40842676734d82
bryant-the-coder <bryantfoo99@gmail.com>
2022-06-21 20:46:45 +0800

    Added lapce

---
+++ lua/themes/lapce-base16.lua
@@ -0,0 +1,18 @@
+return {
+    base00 = "#282c34",
+    base01 = "#2e323a",
+    base02 = "#50545c",
+    base03 = "#646870",
+    base04 = "#6c7078",
+    base05 = "#abb2bf",
+    base06 = "#abb2bf",
+    base07 = "#abb2bf",
+    base08 = "#c678dd",
+    base09 = "#d19a66",
+    base0A = "#e5c07b",
+    base0B = "#98c397",
+    base0C = "#c678dd",
+    base0D = "#abb2bf",
+    base0E = "#61afef",
+    base0F = "#e06c75",
+}
