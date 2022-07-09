786a1d224b103c71c24a7dc1c1968ee690deb3f5
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-09 15:06:35 +0800

    added macchiator colors

---
+++ lua/themes/macchiato-base16.lua
@@ -0,0 +1,20 @@
+local base = {
+    base00 = "#24273a", -- base
+    base01 = "#363a4f", -- surface0
+    base02 = "#494d64", -- surface1
+    base03 = "#5b6078", -- surface2
+    base04 = "#b7bdf8", -- lavender
+    base05 = "#cad3f5", -- text
+    base06 = "#cad3f5", -- text
+    base07 = "#cad3f5", -- text
+    base08 = "#f0c6c6", -- flamingo
+    base09 = "#f5a97f", -- peach
+    base0A = "#f5bde6", -- pink
+    base0B = "#a6da95", -- green
+    base0C = "#f5bde6", -- pink
+    base0D = "#8aadf4", -- blue
+    base0E = "#f5bde6", -- pink
+    base0F = "#8aadf4", -- blue
+}
+
+return base
