86437a5ee0d8165d29688aa342d4758876153d71
bryant-the-coder <bryantfoo99@gmail.com>
2022-07-09 14:16:11 +0800

    added mocha theme

---
+++ lua/themes/mocha-base16.lua
@@ -0,0 +1,20 @@
+local base = {
+    base00 = "#1e1e2e",
+    base01 = "#313244",
+    base02 = "#45475a",
+    base03 = "#585b70",
+    base04 = "#6c7086",
+    base05 = "#cdd6f4",
+    base06 = "#bac2de",
+    base07 = "#a6adc8",
+    base08 = "#f38ba8",
+    base09 = "#fab387",
+    base0A = "#f9e2af",
+    base0B = "#a6e3a1",
+    base0C = "#74c7ec",
+    base0D = "#89b4fa",
+    base0E = "#cba6f7",
+    base0F = "#eba0ac",
+}
+
+return base
