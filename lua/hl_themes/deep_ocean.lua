7adb40a2a1182451fcf8cd439ec954278df1477a
bryant-the-coder <bryantfoo99@gmail.com>
2022-08-17 20:04:09 +0800

    added deep_ocean

---
+++ lua/hl_themes/deep_ocean.lua
@@ -0,0 +1,34 @@
+local colors = {
+    white = "#B0BEC5",
+    darker_black = "#1b1f27",
+    black = "#1e222a", --  nvim bg
+    black2 = "#252931",
+    one_bg = "#282c34", -- real bg of onedark
+    one_bg2 = "#353b45",
+    one_bg3 = "#373b43",
+    grey = "#42464e",
+    grey_fg = "#565c64",
+    grey_fg2 = "#6f737b",
+    light_grey = "#6f737b",
+    baby_pink = "#F07178",
+    red = "#FF5370",
+    pink = "#FF9CAC",
+    line = "#31353d", -- for lines like vertsplit
+    green = "#C3E88D",
+    vibrant_green = "#ABCF76",
+    nord_blue = "#B0C9FF",
+    blue = "#82AAFF",
+    yellow = "#FFCB6B",
+    sun = "#EBCB8B",
+    purple = "#C792EA",
+    dark_purple = "#b37ed6",
+    teal = "#519ABA",
+    orange = "#F78C6C",
+    cyan = "#89DDFF",
+    statusline_bg = "#22262e",
+    lightbg = "#2d3139",
+    pmenu_bg = "#6E98EB",
+    folder_bg = "#6E98EB",
+}
+
+return colors
---
+++ lua/themes/deep_ocean-base16.lua
@@ -0,0 +1,20 @@
+local base = {
+    base00 = "#0F111A",
+    base01 = "#474747",
+    base02 = "#3e4451",
+    base03 = "#464B5D",
+    base04 = "#545862",
+    base05 = "#abb2bf",
+    base06 = "#b6bdca",
+    base07 = "#c8ccd4",
+    base08 = "#FF5370",
+    base09 = "#F78C6C",
+    base0A = "#FFCB6B",
+    base0B = "#C3E88D",
+    base0C = "#B0C9FF",
+    base0D = "#82AAFF",
+    base0E = "#C792EA",
+    base0F = "#F07178",
+}
+
+return base
