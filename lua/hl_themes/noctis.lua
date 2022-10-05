6362f437978c786ed60cf44dfbe620242e576f7e
bryant-the-coder <foobryant99@gmail.com>
2022-10-05 19:25:24 +0800

    added noctis theme

---
+++ lua/hl_themes/noctis.lua
@@ -0,0 +1,35 @@
+local colors = {
+    white = "#cdd6f4",
+    darker_black = "#11111b",
+    black = "#1e1e2e", -- nvim bg
+    black2 = "#1a2124",
+    one_bg = "#313244",
+    one_bg2 = "#45475a",
+    one_bg3 = "#585b70",
+    grey = "#6c7086",
+    grey_fg = "#7f849c",
+    grey_fg2 = "#9399b2",
+    light_grey = "#585b70",
+    red = "#f38ba8",
+    baby_pink = "#eba0ac",
+    pink = "#f5c2e7",
+    line = "#22292b", -- for lines like vertsplit
+    green = "#16b673",
+    vibrant_green = "#49e9a6",
+    nord_blue = "#5b858b",
+    blue = "#507b95",
+    yellow = "#d5971a",
+    sun = "#e0ba63",
+    purple = "#c47fd5",
+    dark_purple = "#b570c6",
+    teal = "#49ace9",
+    orange = "#e66533",
+    cyan = "#49d6e9",
+    statusline_bg = "#1c2325",
+    lightbg = "#2a3133",
+    lightbg2 = "#232a2c",
+    pmenu_bg = "#8ccf7e",
+    folder_bg = "#6da4cd",
+}
+
+return colors
