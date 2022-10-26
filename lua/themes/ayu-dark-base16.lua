850e1a269d13666dd7776c7abda1c6f22304a5eb
bryant-the-coder <foobryant99@gmail.com>
2022-10-26 14:41:28 +0800

    fix ayu

--- lua/themes/ayu-dark-base16.lua
+++ lua/themes/ayu-dark-base16.lua
@@ -7,14 +7,14 @@ local base = {
     base05 = "#c9c7be",
     base06 = "#E6E1CF",
     base07 = "#D9D7CE",
-    base08 = "#c9c7be",
-    base09 = "#FFEE99",
-    base0A = "#56c3f9",
-    base0B = "#AAD84C",
-    base0C = "#FFB454",
-    base0D = "#F07174",
-    base0E = "#FFB454",
-    base0F = "#CBA6F7",
+    base08 = "#F07178",
+    base09 = "#FF8F40",
+    base0A = "#E6B450",
+    base0B = "#C2D94C",
+    base0C = "#39BAE6",
+    base0D = "#59C2FF",
+    base0E = "#CBA6F7",
+    base0F = "#D96C75",
 }
 
 return base
