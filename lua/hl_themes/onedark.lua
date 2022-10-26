d3cf9d8b14940b0649510bf275688ccf252202c1
bryant-the-coder <foobryant99@gmail.com>
2022-10-26 12:25:46 +0800

    fix onedark

--- lua/hl_themes/onedark.lua
+++ lua/hl_themes/onedark.lua
@@ -1,33 +1,48 @@
 local colors = {
+    -- base:    #282c34
+    -- mono-1:  #abb2bf
+    -- mono-2:  #818896
+    -- mono-3:  #5c6370
+    -- hue-1:   #56b6c2
+    -- hue-2:   #61aeee
+    -- hue-3:   #c678dd
+    -- hue-4:   #98c379
+    -- hue-5:   #e06c75
+    -- hue-5-2: #be5046
+    -- hue-6:   #d19a66
+    -- hue-6-2: #e6c07b
     white = "#abb2bf",
     darker_black = "#1b1f27",
     black = "#1e222a", --  nvim bg
     black2 = "#252931",
     one_bg = "#282c34", -- real bg of onedark
     one_bg2 = "#353b45",
-    one_bg3 = "#373b43",
+    one_bg3 = "#30343c",
     grey = "#42464e",
     grey_fg = "#565c64",
     grey_fg2 = "#6f737b",
     light_grey = "#6f737b",
-    red = "#e06c75",
+    red = "#d47d85",
     baby_pink = "#DE8C92",
     pink = "#ff75a0",
-    line = "#31353d", -- for lines like vertsplit
-    green = "#98c379",
+    line = "#2a2e36", -- for lines like vertsplit
+    green = "#A3BE8C",
+    dark_green = "#8aa573",
     vibrant_green = "#7eca9c",
     nord_blue = "#81A1C1",
     blue = "#61afef",
+    dark_blue = "#4896d6",
     yellow = "#e7c787",
     sun = "#EBCB8B",
-    purple = "#de98fd",
-    dark_purple = "#c882e7",
+    purple = "#c882e7",
+    dark_purple = "#af69ce",
     teal = "#519ABA",
-    orange = "#fca2aa",
+    orange = "#d19a66",
     cyan = "#a3b8ef",
     statusline_bg = "#22262e",
     lightbg = "#2d3139",
-    pmenu_bg = "#61afef",
+    lightbg2 = "#262a32",
+    pmenu_bg = "#A3BE8C",
     folder_bg = "#61afef",
 }
 
