9eb8e6b8517c5b483afc7ed68e9de436725fed09
bryant-the-coder <foobryant99@gmail.com>
2022-11-20 22:42:29 +0800

    fix everblush

--- lua/themes/everblush-base16.lua
+++ lua/themes/everblush-base16.lua
@@ -1,20 +1,20 @@
 local base = {
-    base00 = "#141b1e",
-    base01 = "#232a2d",
+    base00 = "#181f21",
+    base01 = "#22292b",
     base02 = "#363d3f",
-    base03 = "#545b5d",
+    base03 = "#404749",
     base04 = "#4f5658",
     base05 = "#cacaca",
     base06 = "#d2d2d2",
     base07 = "#dadada",
     base08 = "#e06e6e",
     base09 = "#e19d5c",
-    base0A = "#e5c76b",
+    base0A = "#e0ba63",
     base0B = "#8ccf7e",
-    base0C = "#67b0e8",
+    base0C = "#95ccf5",
     base0D = "#77aed7",
     base0E = "#c47fd5",
-    base0F = "#E57373",
+    base0F = "#ef7d7d",
 }
 
 return base
