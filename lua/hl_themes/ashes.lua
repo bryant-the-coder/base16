b78920f2eaa033def402d48d6149bf3a8593ad85
bryant-the-coder <foobryant99@gmail.com>
2023-01-10 17:24:14 +0800

    add theme

---
+++ lua/hl_themes/ashes.lua
@@ -0,0 +1,34 @@
+local colors = {
+    white = "#c7ccd1",
+    darker_black = "#161a1d",
+    black = "#1c2023", --  nvim bg
+    black2 = "#24282b",
+    one_bg = "#272b2e", -- real bg of onedark
+    one_bg2 = "#303437",
+    one_bg3 = "#3a3e41",
+    grey = "#44484b",
+    grey_fg = "#4a4e51",
+    grey_fg2 = "#515558",
+    light_grey = "#565a5d",
+    red = "#c79595",
+    baby_pink = "#d09eb7",
+    pink = "#c795ae",
+    line = "#303437", -- for lines like vertsplit
+    green = "#aec795",
+    vibrant_green = "#95c7ae",
+    nord_blue = "#8ca5be",
+    blue = "#95aec7",
+    yellow = "#c7c795",
+    sun = "#d0d09e",
+    purple = "#ae95c7",
+    dark_purple = "#a58cbe",
+    teal = "#8fb4b5",
+    orange = "#c7ae95",
+    cyan = "#9eb7d0",
+    statusline_bg = "#24282b",
+    lightbg = "#303437",
+    pmenu_bg = "#99c366",
+    folder_bg = "#8ca5be",
+}
+
+return colors
