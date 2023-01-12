dc3c1f05e8796947590b80acec55b0ddd8ab4861
bryant-the-coder <foobryant99@gmail.com>
2023-01-13 04:15:02 +0800

    purify theme

---
+++ lua/themes/purify-base16.lua
@@ -0,0 +1,20 @@
+local base = {
+    base00 = "#151F29",
+    base01 = "#252F3C",
+    base02 = "#313B48",
+    base03 = "#3D4754",
+    base04 = "#565C64",
+    base05 = "#EBEBEB",
+    base06 = "#F5F5F5",
+    base07 = "#FAFAFA",
+    base08 = "#FF79C6",
+    base09 = "#FF875F",
+    base0A = "#FFFF87",
+    base0B = "#5FFF87",
+    base0C = "#69C3FF",
+    base0D = "#5FAFFF",
+    base0E = "#AF87FF",
+    base0F = "#FF6059",
+}
+
+return base
