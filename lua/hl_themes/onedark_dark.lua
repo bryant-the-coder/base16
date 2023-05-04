d3270a09ce373ef154d6e28630e142c3e67d7d8d
bryant-the-coder <foobryant99@gmail.com>
2023-05-04 22:23:45 +0800

    onedark-dark

---
+++ lua/hl_themes/onedark_dark.lua
@@ -0,0 +1,37 @@
+local colors = {
+    white = "#abb2bf",
+    darker_black = "#1b1f27",
+    black = "#000000", --  nvim bg
+    black2 = "#252931",
+    one_bg = "#282c34", -- real bg of onedark
+    one_bg2 = "#353b45",
+    one_bg3 = "#30343c",
+    grey = "#7f848e",
+    grey_fg = "#565c64",
+    grey_fg2 = "#6f737b",
+    light_grey = "#6f737b",
+    red = "#ef596f",
+    baby_pink = "#DE8C92",
+    pink = "#ff75a0",
+    line = "#2a2e36", -- for lines like vertsplit
+    green = "#89ca78",
+    dark_green = "#8aa573",
+    vibrant_green = "#7eca9c",
+    nord_blue = "#81A1C1",
+    blue = "#61afef",
+    dark_blue = "#4896d6",
+    yellow = "#e5c07b",
+    sun = "#EBCB8B",
+    purple = "#d55fde",
+    dark_purple = "#af69ce",
+    teal = "#519ABA",
+    orange = "#d19a66",
+    cyan = "#2bbac5",
+    statusline_bg = "#22262e",
+    lightbg = "#2d3139",
+    lightbg2 = "#262a32",
+    pmenu_bg = "#A3BE8C",
+    folder_bg = "#61afef",
+}
+
+return colors
---
+++ lua/themes/onedark_dark-base16.lua
@@ -0,0 +1,23 @@
+local base = {
+    base00 = "#000000",
+    -- base00 = "#282c34", -- og
+    base01 = "#353b45",
+    base02 = "#3e4451",
+    base03 = "#434852",
+    base04 = "#7f848e",
+    base05 = "#abb2bf",
+    base06 = "#b6bdca",
+    base07 = "#c8ccd4",
+    base08 = "#ef596f",
+    base09 = "#d19a66",
+    base0A = "#e5c07b",
+    -- base0A = "#e6c07b", -- og
+    base0B = "#89ca78",
+    base0C = "#2bbac5",
+    base0D = "#61afef",
+    -- base0D = "#61aeee", -- og
+    base0E = "#d55fde",
+    base0F = "#be5046",
+}
+
+return base
